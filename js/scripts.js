6f7663c224223a7d77a69a124dcee204aa515756
nuria <raquellopezsancheez@gmail.com>
2023-08-07 11:33:04 +0200

    7

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-window.addEventListener('scroll', function () {
-  let elements = document.getElementsByClassName('scroll-content');
-  let screenSize = window.innerHeight;
-
-  for (var i = 0; i < elements.length; i++) {
-    let element = elements[i];
-
-    if (element.getBoundingClientRect().top < screenSize) {
-      element.classList.add('visible');
-    } else {
-      element.classList.remove('visible');
-    }
-  }
-});
-
 function validar(event) {
   event.preventDefault();
   console.log('dentro...');
